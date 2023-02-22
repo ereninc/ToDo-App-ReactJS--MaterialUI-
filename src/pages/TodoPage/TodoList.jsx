74f5aeeef66c974437537240ee8de2819f59d284
Eren <erencan_inanci@hotmail.com>
2023-02-23 01:37:18 +0300

    icon package, buttons, text decoration style update

--- src/pages/TodoPage/TodoList.jsx
+++ src/pages/TodoPage/TodoList.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Paper, List, ListItem, ListItemText, Divider } from "@mui/material";
+import { Paper, List, Divider } from "@mui/material";
+import TodoItem from "./components/TodoItem";
+import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList(props) {
   return (
     <Paper>
       <List>
         {props.todos.map((todo) => (
-          <>
-            <ListItem key={todo.id}>
-              <ListItemText>{todo.task}</ListItemText>
-            </ListItem>
+          <div key={uuidv4()}>
+            <TodoItem task={todo.task} completed={todo.completed} />
             <Divider />
-          </>
+          </div>
         ))}
       </List>
     </Paper>
--- src/pages/TodoPage/components/TodoItem.jsx
+++ src/pages/TodoPage/components/TodoItem.jsx
@@ -1,5 +1,32 @@
+import {
+  ListItem,
+  ListItemText,
+  Checkbox,
+  IconButton,
+  ListItemSecondaryAction,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import React from "react";
 
-export default function TodoItem() {
-  return <div>TodoItem</div>;
+export default function TodoItem({ task, completed }) {
+  return (
+    <ListItem>
+      <Checkbox tabIndex={-1} checked={completed} />
+      <ListItemText
+        style={{ textDecoration: completed ? "line-through" : "none" }}
+      >
+        <span>{task}</span>
+      </ListItemText>
+
+      <ListItemSecondaryAction>
+        <IconButton aria-label="Delete">
+          <DeleteIcon />
+        </IconButton>
+        <IconButton aria-label="Edit">
+          <EditIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
 }
